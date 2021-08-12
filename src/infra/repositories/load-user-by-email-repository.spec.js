60f571fd3726eb3b8fc2cf808d713b4e87a2778f
Jeverson <jeversontp@gmail.com>
2021-08-12 08:11:50 -0300

    returns a user a if user is found

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,13 +1,49 @@
+const { MongoClient } = require('mongodb')
+
 class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
   async load (email) {
-    return null
+    const user = await this.userModel.findOne({ email })
+    return user
   }
 }
 
 describe('LoadUserByEmailRepository', () => {
-  test('Shoul return null if no user is found', async () => {
-    const sut = new LoadUserByEmailRepository()
+  let client, db
+
+  beforeAll(async () => {
+    client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    db = client.db()
+  })
+
+  afterAll(async () => {
+    await client.close()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  test('Should return null if no user is found', async () => {
+    const userModel = db.collection('users')
+    const sut = new LoadUserByEmailRepository(userModel)
     const user = await sut.load('invalid_email@mail.com')
-    expect(user).toBeNull()
+    expect(user).toBeUndefined()
+  })
+
+  test('Should return an user if user is found', async () => {
+    const userModel = db.collection('users')
+    await userModel.insertOne({
+      email: 'valid_email@mail.com'
+    })
+    const sut = new LoadUserByEmailRepository(userModel)
+    const user = await sut.load('valid_email@mail.com')
+    expect(user.email).toBe('valid_email@mail.com')
   })
 })

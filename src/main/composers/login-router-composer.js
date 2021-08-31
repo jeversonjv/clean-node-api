fcd1a15a1750399bccc6b3c086a19d3f45f1eca5
Jeverson <jeversontp@gmail.com>
2021-08-30 22:19:05 -0300

    move loginRouter composition to its own file

---
+++ src/main/composers/login-router-composer.js
@@ -0,0 +1,25 @@
+const LoginRouter = require('../../presentation/routers/login-router')
+const AuthUseCase = require('../../domain/usecases/auth-usecase')
+const EmailValidator = require('../../util/helpers/email-validator')
+const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
+const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
+const Encrypter = require('../../util/helpers/encrypter')
+const TokenGenerator = require('../../util/helpers/token-generator')
+const env = require('../config/env')
+
+const tokenGenerator = new TokenGenerator(env.tokenSecret)
+const encrypter = new Encrypter()
+const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+const loadUserByEmailRepository = new LoadUserByEmailRepository()
+const emailValidator = new EmailValidator()
+const authUseCase = new AuthUseCase({
+  loadUserByEmailRepository,
+  updateAccessTokenRepository,
+  encrypter,
+  tokenGenerator
+})
+const loginRouter = new LoginRouter({
+  authUseCase, emailValidator
+})
+
+module.exports = loginRouter
--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,3 +1,4 @@
+const loginRouter = require('../composers/login-router-composer')
 module.exports = router => {
-  router.post('/login', () => {})
+  router.post('/login', loginRouter)
 }
